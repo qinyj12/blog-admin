4993f237bd611b3acc36a7f68d301b73b404b01d
qinyj12 <qinyj12@gmail.com>
2021-01-15 19:45:51 +0800

    继续完成用户列表页

--- src/views/users/index.vue
+++ src/views/users/index.vue
@@ -2,20 +2,46 @@
     <div class="users-container">
         <div class="user-title">users</div>
 
-        <el-table
-            :data="tableData"
-            border
-            class="users-table"
-        >
+        <el-table :data="tableData" border class="users-table">
             <el-table-column prop="id" label="ID" width="50"></el-table-column>
-
-            <el-table-column prop="name" label="昵称" ></el-table-column>
+            <el-table-column prop="nickName" label="昵称"></el-table-column>
+            <el-table-column prop="avatar" label="头像">
+                <template slot-scope="scope">
+                    <img :src="scope.row.avatar" />
+                </template>
+            </el-table-column>
+            <el-table-column prop="jurisdiction" label="权限"></el-table-column>
+            <el-table-column prop="phone" label="手机号"></el-table-column>
+            <el-table-column prop="addTime" label="加入日期"></el-table-column>
+            <el-table-column prop="recentlyActive" label="最近活跃"></el-table-column>
+            <el-table-column prop="articleNum" label="博客数量" width="80"></el-table-column>
+            <el-table-column prop="operation" label="操作" width="60">
+                <template slot-scope="scope">
+                    <el-button @click="ViewUser(scope.row)" type="text">查看</el-button>
+                </template>
+            </el-table-column>
         </el-table>
     </div>
 </template>
 <script>
 export default {
-    tableData: [],
+    data() {
+        return {
+            tableData: [
+                {
+                    id: '0', 
+                    nickName: 'dog', 
+                    avatar: 'https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif?imageView2/1/w/80/h/80', 
+                    jurisdiction: 'admin',
+                    phone: '17611595223',
+                    addTime: '2021', 
+                    recentlyActive: '2021', 
+                    articleNum: '1'
+                } 
+            ],
+        }
+    },
+    
 }
 </script>
 
